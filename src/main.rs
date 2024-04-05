15766bc8198392f2f352eae95fa5a2c2b38ab4b2
bugwiper <pascbet@cmtcdmxy3wy0f7t.rd.corpintra.net>
2024-04-05 11:07:51 +0200

    ICAO code is now taken from user input. User can exit the program by entering EXIT. Input is currently only checked for correct length, other invalid entries or unknown airports still lead to panic -> Todo

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,81 @@
 mod libs;
+use std::io;
 
 
 #[tokio::main]
 async fn main() {
-
-    let icao: String;
-    let metar: String;
-    let station: String;
-    let wind: String;
-
-
-    /* Determine ICAO code for target airport */
-    icao = "EDTY".to_string();
-
     
-    /* Determine station name */
-    
-    station = libs::station::read_station_info(&icao).await;
+    loop {  
 
-    /* Determine metar for station */
-    metar = libs::metar::read_metar_text(&icao).await;
+        let icao: String;
+        let metar: String;
+        let station: String;
+        let wind: String;
 
-    /* Determine wind for station */
-    wind = libs::metar::get_wind(&icao).await;
+        /* Get user input for ICAO code */
+        println!("Please enter ICAO code for your target airport:");
+        println!("(e.g. EDDS for Stuttgart airport)");
+        println!("(Type EXIT to exit)");
+
+        
+        // Create a mutable String to store the user input
+        let mut input = String::new();
 
 
-    /* Print result */
-    println!("The current METAR for {} is {}", station, metar);
-    println!("");
-    println!("The wind speed is {}kph!", wind);
+
+        // Read user input from the command line
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        let trimmed = &input[..input.len() - 1];
+        let new: String = trimmed.to_string();
 
 
+        /* Validate exit statement */
+        if &new == "EXIT" || &new == "exit" {
+            break;
+        }
+
+        /* Validate user input */
+        if !validate_user_input(&new){
+            println! ("Invalid ICAO code");
+            continue;
+        }
+
+        /* Determine ICAO code for target airport */
+        icao = new;
+
+        
+        /* Determine station name */
+        
+        station = libs::station::read_station_info(&icao).await;
+
+        /* Determine metar for station */
+        metar = libs::metar::read_metar_text(&icao).await;
+
+        /* Determine wind for station */
+        wind = libs::metar::get_wind(&icao).await;
+
+
+        /* Print result */
+        println!("The current METAR for {} is {}", station, metar);
+        println!("");
+        println!("The wind speed is {}kph!", wind);
+        println!("");
+        println!("");
+
+}
+
+
+}
+
+fn validate_user_input(input: &String) -> bool {
+
+    if input.len() == 4 {
+        return true 
+    } else {
+        return false
+    }
 }
 
 
